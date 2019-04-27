9d1631cd440b0617c411648ec8e5b663e181c08a
Alan <alan.moya@alumnos.duoc.cl>
2019-04-26 23:57:09 -0400

    ferme configurado sin confirmar http://www.cantabriatic.com/configurar-un-repositorio-git-en-visual-studio-2015/

---
+++ capaLogicaNegocio/CL_Contexto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using capaDALC;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace capaLogicaNegocio
+{
+    public class CL_Contexto
+    {
+        private capaDALC.Entities conexion;
+
+        public Entities Entidades
+        {
+            get
+            {
+                if (conexion == null)
+                {
+                    conexion = new capaDALC.Entities();
+                }
+                return conexion;
+            }
+        }
+        public CL_Contexto(){}
+    }
+}
